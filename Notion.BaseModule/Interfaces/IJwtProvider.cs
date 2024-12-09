30d753c5b04fd53526343156e8dcef2c0ec80fa9
IvanZhenin <ivanzhen1n@mail.ru>
2024-12-09 14:49:14 +0500

    Add jwt-provider

---
+++ Notion.BaseModule/Interfaces/IJwtProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Notion.BaseModule.Interfaces
+{
+    public interface IJwtProvider
+    {
+        public string GenerateToken(int userId, string userLogin);
+    }
+}
---
+++ Notion.BusinessLogic/Services/JwtProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Notion.BaseModule.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Notion.BusinessLogic.Services
+{
+    public class JwtProvider : IJwtProvider
+    {
+        public string GenerateToken(int userId, string userLogin)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, userLogin),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperSecretMyKey777SuperSecretMyKey777"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: creds);
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
+        }
+    }
+}
