4596b5394689b8a19dcfbc7927e7b0de735a1016
IvanZhenin <ivanzhen1n@mail.ru>
2024-12-09 16:54:45 +0500

    Rebuild some struct interfaces

--- Notion.BaseModule/Interfaces/INotionService.cs
+++ Notion.BaseModule/Interfaces/INotionService.cs
@@ -5,8 +5,8 @@ namespace Notion.BaseModule.Interfaces
 {
     public interface INotionService
     {
-        public Task<IEnumerable<UserNotion>> GetNotionListByUserIdAsync(int userId);
-        public Task<UserNotion> AddNotionAsync(int userId, string text);
+        public Task<IEnumerable<UserNotion>> GetNotionListByUserLoginAsync(string userLogin);
+        public Task<UserNotion> AddNotionAsync(string userLogin, string text);
         public Task<UserNotion> ChangeNotionAsync(Guid notionId, string? newText, bool? isComplited);
         public Task DeleteNotionAsync(Guid notionId);
     }
--- Notion.BaseModule/Interfaces/IUserService.cs
+++ Notion.BaseModule/Interfaces/IUserService.cs
@@ -5,6 +5,7 @@ namespace Notion.BaseModule.Interfaces
 {
     public interface IUserService
     {
+        public Task<User> GetUserByIdAsync(int userId);
         public Task<User> GetUserByLoginAsync(string login);
         public Task<bool> CheckUserDataAsync(string login, string password);
         public Task<User> CreateUserAsync(string login, string password);
